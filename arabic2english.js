178f34e75abada1c9424e57aefe92e66cd37b791
Jeffrey Bulanadi <jeffrey.bulanadi@gmail.com>
2019-07-15 02:28:18 +0800

    Added new initial implementation from 1 to 19

--- arabic2english.js
+++ arabic2english.js
@@ -1,39 +1,50 @@
+/*
+ * I've thought of a better way to do this assignment.
+ *
+ * My original though was to iterate through each of the number and
+ * determine if it's a ones, tens, hundreds, etc.
+ * But as I was trying it out, the conditions were getting more and more
+ * complicated so I had to stop and think of a different solution.
+ *
+ * Looking at this problem, we can say that there's a pattern:
+ * For example: 123,456,789
+ *
+ * In this we can "group" together numbers "123", "456", and "789"
+ * Given this scenario, we can say that:
+ *
+ * The first group is a "hundreds"
+ * The second group is a "thousands" with a possibility of a "hundreds"
+ * The third group is the "millions" and also with a possibility of "hundreds"
+ *
+ * Given that fact, we can say that "hundreds" should be made available to all "groups"
+ * and after that, we can label them with "thousand", "million", "billion", etc
+ *
+ * Also, by grouping the numbers together, we can shorten the iteration
+ */
+
+
 const getEnglishValue = (param = process.argv[2]) => {
     let englishValue = '';
     param = param.split('').reverse().join('');
 
-    // Check for non-number entries???
-    for (let iter = 0; iter < param.length; iter++) {
-        // Counter should reset after 3 counts to separate hundreds, thousands, millions, etc.
-        let counter = 1;
-        const currentNumber = parseInt(param.charAt(iter));
-        const prevNumber = parseInt(param.charAt(iter - 1));
-        const nextNumber = parseInt(param.charAt(iter + 1));
+    for (let i = 0; i < Math.ceil(param.length / 3); i ++) {
+        const group = param.substr(i * 3, 3);
+        const firstNumber = parseInt(group[0]);
+        // We have to set a default value of 0 to second and third numbers
+        // for scenarios like a single digit number where the parsed value is NaN
+        const secondNumber = parseInt(group[1] || '0');
+        const thirdNumber = parseInt(group[2] || '0');
+        const firstTwoNumbers = parseInt(`${secondNumber}${firstNumber}`);
 
-        let numberToCheck = parseInt(`${nextNumber}${currentNumber}`);
-
-        // If two digit number
-        if (nextNumber <= 1 && counter < 2) {
-            if (nextNumber < 2) {
-                englishValue = `${getTenToNineteen(numberToCheck)} ${englishValue}`;
-            } else {
-                englishValue = `${getTens(nextNumber)} ${getSingleDigitValue(currentNumber)} ${englishValue}`;
-            }
-            iter++;
-            counter++;
-        } else {
-            // Single digit number
-            // Hundreds should also be placed here
-            englishValue = `${getSingleDigitValue(currentNumber)} ${(iter + 1) % 3 === 0 ?  'hundred' : ''} ${englishValue}`;
+        // Check if single number (1 to 9)
+        if (firstTwoNumbers < 10) {
+            englishValue = `${getSingleDigitValue(firstNumber)} ${englishValue}`;
+        } else if (firstTwoNumbers >= 10 && firstTwoNumbers < 20) {
+            englishValue = `${getTenToNineteen(firstTwoNumbers)} ${englishValue}`;
         }
 
-        counter++;
-        if (counter === 4) {
-            counter = 1;
-        }
     }
 
-    // Remove all extra spaces
     console.log(englishValue.replace(/\s{2}/g, ' ').trim());
     return englishValue.replace(/\s{2}/g, ' ').trim();
 };
