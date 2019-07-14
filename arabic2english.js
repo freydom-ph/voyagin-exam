9c1e9aa78e44828d1b7497e6b0f799ff1739b342
Jeffrey Bulanadi <jeffrey.bulanadi@gmail.com>
2019-07-15 06:51:41 +0800

    Refactored code
    Replaced function switch case with an array as it would be shorter
    and faster to have the program access the array instead of having a
    function run just to retrieve a simple string.

--- arabic2english.js
+++ arabic2english.js
@@ -25,6 +25,13 @@
 
 const getEnglishValue = (param = process.argv[2]) => {
     let englishValue = '';
+    let oneToNineteen = ['one', "two", 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten',
+        'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+
+    let tens = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+    let extensions = ['thousand', 'million', 'billion', 'trillion'];
+
     param = param.split('').reverse().join('');
 
     for (let i = 0; i < Math.ceil(param.length / 3); i ++) {
@@ -37,79 +44,21 @@ const getEnglishValue = (param = process.argv[2]) => {
         const thirdNumber = parseInt(group[2] || '0');
         const firstTwoNumbers = parseInt(`${secondNumber}${firstNumber}`);
 
-        // Check if single number (1 to 9)
-        if (firstTwoNumbers < 10) {
-            tempValue = `${getSingleDigitValue(firstNumber)} ${tempValue}`;
-        } else if (firstTwoNumbers >= 10 && firstTwoNumbers < 20) {
-            tempValue = `${getTenToNineteen(firstTwoNumbers)} ${tempValue}`;
+        if (firstTwoNumbers > 0 && firstTwoNumbers < 20) {
+            tempValue = `${oneToNineteen[firstTwoNumbers - 1]}`;
         } else if (firstTwoNumbers >= 20) {
-            tempValue = `${getTens(secondNumber)} ${getSingleDigitValue(firstNumber) || ''}`;
+            tempValue = `${tens[secondNumber - 2]} ${oneToNineteen[firstNumber - 1] || ''}`;
         }
 
         if (thirdNumber > 0) {
-            tempValue = `${getSingleDigitValue(thirdNumber)} hundred ${tempValue}`;
+            tempValue = `${oneToNineteen[thirdNumber - 1]} hundred ${tempValue}`;
         }
 
-        englishValue = `${tempValue} ${tempValue.trim() ? getExtension(i) || '' : ''} ${englishValue}`;
+        englishValue = `${tempValue} ${tempValue.trim() ? extensions[i - 1] || '' : ''} ${englishValue}`;
     }
 
     console.log(englishValue.replace(/[\s]+/g, ' ').trim());
     return englishValue.replace(/[\s]+/g, ' ').trim();
-};
-
-const getSingleDigitValue = number => {
-    switch(number) {
-        case 1: return 'one';
-        case 2: return 'two';
-        case 3: return 'three';
-        case 4: return 'four';
-        case 5: return 'five';
-        case 6: return 'six';
-        case 7: return 'seven';
-        case 8: return 'eight';
-        case 9: return 'nine';
-        default: return '';
-    }
-};
-
-const getTenToNineteen = number => {
-    switch(number) {
-        case 10: return 'ten';
-        case 11: return 'eleven';
-        case 12: return 'twelve';
-        case 13: return 'thirteen';
-        case 14: return 'fourteen';
-        case 15: return 'fifteen';
-        case 16: return 'sixteen';
-        case 17: return 'seventeen';
-        case 18: return 'eighteen';
-        case 19: return 'nineteen';
-        default: return '';
-    }
-};
-
-const getTens = number => {
-    switch(number) {
-        case 2: return 'twenty';
-        case 3: return 'thirty';
-        case 4: return 'forty';
-        case 5: return 'fifty';
-        case 6: return 'sixty';
-        case 7: return 'seventy';
-        case 8: return 'eighty';
-        case 9: return 'ninety';
-        default: return '';
-    }
-};
-
-const getExtension = position => {
-    switch(position) {
-        case 1: return 'thousand';
-        case 2: return 'million';
-        case 3: return 'billion';
-        case 4: return 'trillion';
-        default: return '';
-    }
 };
 
 module.exports.getEnglishValue = getEnglishValue;
